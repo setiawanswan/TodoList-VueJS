200ec9358fac1261c71fe034560085ca5c8454e2
setiawanswan <setiawan.tumilin@gmail.com>
2019-08-02 14:28:43 +0700

    try to create todo list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="todo-wrapper">
+      <TodoList />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '../components/HelloWorld.vue';
+import TodoList from '../components/TodoList.vue';
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    TodoList,
+    HelloWorld,
   }
 };
 </script>
